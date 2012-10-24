7b8b3983bd49a975f7a79fe1a0a17e5b19e9a22a
Gilles Crofils <gilles@secondbureau.com>
2012-10-24 22:47:17 +0800

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lescarsrouges::Application.config.secret_token = 'ab773506bda37d0edadba648c159817043955a8123b5ba483b23831bcb1348b62bc73de57c330082983c3192ebabaec11f6322b4e8ab7c34914ed62b9b90e3a0'
